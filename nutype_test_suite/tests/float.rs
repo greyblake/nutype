754912f780ccb6c973ac64f8d130a46f13b1e22d
Sergey Potapov <blake131313@gmail.com>
2022-12-04 08:00:53 +0100

    Split number unit tests into float and integer

--- nutype_test_suite/tests/float.rs
+++ nutype_test_suite/tests/float.rs
@@ -1,6 +1,218 @@
 use nutype::nutype;
 
 #[cfg(test)]
+mod sanitizers {
+    use super::*;
+
+    #[test]
+    fn test_clamp() {
+        #[nutype(sanitize(clamp(18.0, 99.0)))]
+        struct Age(f64);
+
+        assert_eq!(Age::new(17.0).into_inner(), 18.0);
+        assert_eq!(Age::new(100.0).into_inner(), 99.0);
+    }
+
+    #[cfg(test)]
+    mod with {
+        use super::*;
+
+        #[test]
+        fn test_with_closure_with_explicit_type() {
+            /// Some documentation for Cent goes here.
+            #[nutype(sanitize(with = |n: f64| n.clamp(0.0, 100.0)))]
+            pub struct Cent(f64);
+
+            assert_eq!(Cent::new(-10.0).into_inner(), 0.0);
+        }
+
+        #[test]
+        fn test_closure_with_no_type() {
+            #[nutype(sanitize(with = |n| n.clamp(0.0, 100.0)))]
+            pub struct Cent(f64);
+
+            assert_eq!(Cent::new(-10.0).into_inner(), 0.0);
+        }
+
+        fn sanitize_cent(value: f64) -> f64 {
+            value.clamp(0.0, 100.0)
+        }
+
+        #[test]
+        fn test_with_function() {
+            #[nutype(sanitize(with = sanitize_cent))]
+            pub struct Cent(f64);
+
+            assert_eq!(Cent::new(222.0).into_inner(), 100.0);
+        }
+    }
+
+    #[test]
+    fn test_from_trait() {
+        #[nutype]
+        #[derive(From)]
+        struct Age(f64);
+
+        assert_eq!(Age::from(17.0).into_inner(), 17.0);
+    }
+}
+
+#[cfg(test)]
+mod validators {
+    use super::*;
+
+    #[test]
+    fn test_min() {
+        #[nutype(validate(min = 18.0))]
+        #[derive(*)]
+        struct Age(f32);
+
+        assert_eq!(Age::new(17.0).unwrap_err(), AgeError::TooSmall);
+        assert_eq!(Age::new(18.0).unwrap().into_inner(), 18.0);
+    }
+
+    #[test]
+    fn test_max() {
+        #[nutype(validate(max = 99.0))]
+        #[derive(*)]
+        struct Age(f32);
+
+        assert_eq!(Age::new(100.0).unwrap_err(), AgeError::TooBig);
+        assert_eq!(Age::new(99.0).unwrap().into_inner(), 99.0);
+    }
+
+    #[test]
+    fn test_min_and_max() {
+        #[nutype(validate(min = 18.0, max = 99.0))]
+        #[derive(*)]
+        struct Age(f32);
+
+        assert_eq!(Age::new(17.9).unwrap_err(), AgeError::TooSmall);
+        assert_eq!(Age::new(99.1).unwrap_err(), AgeError::TooBig);
+        assert_eq!(Age::new(25.0).unwrap().into_inner(), 25.0);
+    }
+
+    #[cfg(test)]
+    mod with {
+        use super::*;
+
+        #[test]
+        fn test_with_closure_with_explicit_type() {
+            #[nutype(validate(with = |&c: &f32| c >= 0.0 && c <= 100.0 ))]
+            #[derive(*)]
+            pub struct Cent(f32);
+
+            assert_eq!(Cent::new(-0.1), Err(CentError::Invalid));
+            assert_eq!(Cent::new(100.1), Err(CentError::Invalid));
+            assert_eq!(Cent::new(100.0).unwrap().into_inner(), 100.0);
+            assert_eq!(Cent::new(0.0).unwrap().into_inner(), 0.0);
+        }
+
+        #[test]
+        fn test_closure_with_no_type() {
+            #[nutype(validate(with = |&c| c >= 0.0 && c <= 100.0 ))]
+            #[derive(*)]
+            pub struct Cent(f32);
+
+            assert_eq!(Cent::new(-0.1), Err(CentError::Invalid));
+            assert_eq!(Cent::new(100.1), Err(CentError::Invalid));
+            assert_eq!(Cent::new(100.0).unwrap().into_inner(), 100.0);
+            assert_eq!(Cent::new(0.0).unwrap().into_inner(), 0.0);
+        }
+
+        fn is_cent_valid(&val: &f32) -> bool {
+            val >= 0.0 && val <= 100.0
+        }
+
+        #[test]
+        fn test_with_function() {
+            #[nutype(validate(with = is_cent_valid))]
+            #[derive(*)]
+            pub struct Cent(f32);
+
+            assert_eq!(Cent::new(-0.1), Err(CentError::Invalid));
+            assert_eq!(Cent::new(100.1), Err(CentError::Invalid));
+            assert_eq!(Cent::new(100.0).unwrap().into_inner(), 100.0);
+            assert_eq!(Cent::new(0.0).unwrap().into_inner(), 0.0);
+        }
+    }
+
+    #[test]
+    fn test_try_from_trait() {
+        #[nutype(validate(min = 18.0))]
+        #[derive(*)]
+        struct Age(f64);
+
+        assert_eq!(Age::try_from(17.9).unwrap_err(), AgeError::TooSmall);
+        assert_eq!(Age::try_from(18.0).unwrap().into_inner(), 18.0);
+    }
+}
+
+#[cfg(test)]
+mod types {
+    use super::*;
+
+    #[test]
+    fn test_f32_validate() {
+        #[nutype(validate(min = 0.0, max = 100))]
+        #[derive(Debug, PartialEq)]
+        pub struct Width(f32);
+
+        assert_eq!(Width::new(-0.0001), Err(WidthError::TooSmall));
+        assert_eq!(Width::new(100.0001), Err(WidthError::TooBig));
+        assert!(Width::new(0.0).is_ok());
+        assert!(Width::new(100.0).is_ok());
+    }
+
+    #[test]
+    fn test_f64_validate() {
+        #[nutype(validate(min = 0.0, max = 100))]
+        #[derive(Debug, PartialEq)]
+        pub struct Width(f64);
+
+        assert_eq!(Width::new(-0.0001), Err(WidthError::TooSmall));
+        assert_eq!(Width::new(100.0001), Err(WidthError::TooBig));
+
+        assert_eq!(Width::new(0.0).unwrap().into_inner(), 0.0);
+
+        let w: f64 = Width::new(100.0).unwrap().into_inner();
+        assert_eq!(w, 100.0);
+    }
+
+    #[test]
+    fn test_f64_negative() {
+        #[nutype(
+            sanitize(clamp(-200.25, -5))
+            validate(min = -100.25, max = -50.1)
+        )]
+        #[derive(*)]
+        pub struct Balance(f64);
+
+        assert_eq!(Balance::new(-300.0), Err(BalanceError::TooSmall));
+        assert_eq!(Balance::new(-4.0), Err(BalanceError::TooBig));
+
+        let balance = Balance::new(-100.24).unwrap();
+        assert_eq!(balance.into_inner(), -100.24);
+    }
+}
+
+#[cfg(test)]
+mod visibility {
+    mod encapsulated {
+        use nutype::nutype;
+
+        #[nutype(sanitize(with = |n: f32| n.clamp(0.0, 100.0)))]
+        pub struct Percentage(f32);
+    }
+
+    #[test]
+    fn test_public_visibility() {
+        let percentage = encapsulated::Percentage::new(133.0);
+        assert_eq!(percentage.into_inner(), 100.0);
+    }
+}
+
+#[cfg(test)]
 mod traits {
     use super::*;
     use nutype_test_suite::test_helpers::traits::*;
@@ -68,11 +280,11 @@ mod traits {
 
         #[nutype]
         #[derive(Borrow)]
-        pub struct Age(u8);
+        pub struct Age(f64);
 
-        let age = Age::new(32);
-        let age_borrowed: &u8 = age.borrow();
-        assert_eq!(age_borrowed, &32);
+        let age = Age::new(32.0);
+        let age_borrowed: &f64 = age.borrow();
+        assert_eq!(age_borrowed, &32.0);
     }
 
     #[test]
