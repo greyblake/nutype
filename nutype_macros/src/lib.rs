6047b2eaec4785330248dcaea2a4562629641172
Sergey Potapov <blake131313@gmail.com>
2023-07-02 10:27:04 +0200

    Get rid of nested patter matching

--- nutype_macros/src/lib.rs
+++ nutype_macros/src/lib.rs
@@ -26,6 +26,9 @@ fn expand_nutype(
     attrs: TokenStream,
     type_definition: TokenStream,
 ) -> Result<TokenStream, syn::Error> {
+    use IntegerInnerType::*;
+    use FloatInnerType::*;
+
     let NewtypeMeta {
         doc_attrs,
         type_name,
@@ -34,55 +37,21 @@ fn expand_nutype(
         derive_traits,
     } = parse_meta(type_definition)?;
     match inner_type {
+        // TODO: Oh oh.. Try to DRY the repetition
         InnerType::String => StringNewtype::expand(attrs, doc_attrs, type_name, vis, derive_traits),
-        InnerType::Integer(tp) => {
-            // TODO: Oh oh.. Try to DRY the repetition
-            match tp {
-                IntegerInnerType::U8 => {
-                    IntegerNewtype::<u8>::expand(attrs, doc_attrs, type_name, vis, derive_traits)
-                }
-                IntegerInnerType::U16 => {
-                    IntegerNewtype::<u16>::expand(attrs, doc_attrs, type_name, vis, derive_traits)
-                }
-                IntegerInnerType::U32 => {
-                    IntegerNewtype::<u32>::expand(attrs, doc_attrs, type_name, vis, derive_traits)
-                }
-                IntegerInnerType::U64 => {
-                    IntegerNewtype::<u64>::expand(attrs, doc_attrs, type_name, vis, derive_traits)
-                }
-                IntegerInnerType::U128 => {
-                    IntegerNewtype::<u128>::expand(attrs, doc_attrs, type_name, vis, derive_traits)
-                }
-                IntegerInnerType::Usize => {
-                    IntegerNewtype::<usize>::expand(attrs, doc_attrs, type_name, vis, derive_traits)
-                }
-                IntegerInnerType::I8 => {
-                    IntegerNewtype::<i8>::expand(attrs, doc_attrs, type_name, vis, derive_traits)
-                }
-                IntegerInnerType::I16 => {
-                    IntegerNewtype::<i16>::expand(attrs, doc_attrs, type_name, vis, derive_traits)
-                }
-                IntegerInnerType::I32 => {
-                    IntegerNewtype::<i32>::expand(attrs, doc_attrs, type_name, vis, derive_traits)
-                }
-                IntegerInnerType::I64 => {
-                    IntegerNewtype::<i64>::expand(attrs, doc_attrs, type_name, vis, derive_traits)
-                }
-                IntegerInnerType::I128 => {
-                    IntegerNewtype::<i128>::expand(attrs, doc_attrs, type_name, vis, derive_traits)
-                }
-                IntegerInnerType::Isize => {
-                    IntegerNewtype::<isize>::expand(attrs, doc_attrs, type_name, vis, derive_traits)
-                }
-            }
-        }
-        InnerType::Float(tp) => match tp {
-            FloatInnerType::F32 => {
-                FloatNewtype::<f32>::expand(attrs, doc_attrs, type_name, vis, derive_traits)
-            }
-            FloatInnerType::F64 => {
-                FloatNewtype::<f64>::expand(attrs, doc_attrs, type_name, vis, derive_traits)
-            }
-        },
+        InnerType::Integer(U8) => IntegerNewtype::<u8>::expand(attrs, doc_attrs, type_name, vis, derive_traits),
+        InnerType::Integer(U16) => IntegerNewtype::<u16>::expand(attrs, doc_attrs, type_name, vis, derive_traits),
+        InnerType::Integer(U32) => IntegerNewtype::<u32>::expand(attrs, doc_attrs, type_name, vis, derive_traits),
+        InnerType::Integer(U64) => IntegerNewtype::<u64>::expand(attrs, doc_attrs, type_name, vis, derive_traits),
+        InnerType::Integer(U128) => IntegerNewtype::<u128>::expand(attrs, doc_attrs, type_name, vis, derive_traits),
+        InnerType::Integer(Usize) => IntegerNewtype::<usize>::expand(attrs, doc_attrs, type_name, vis, derive_traits),
+        InnerType::Integer(I8) => IntegerNewtype::<i8>::expand(attrs, doc_attrs, type_name, vis, derive_traits),
+        InnerType::Integer(I16) => IntegerNewtype::<i16>::expand(attrs, doc_attrs, type_name, vis, derive_traits),
+        InnerType::Integer(I32) => IntegerNewtype::<i32>::expand(attrs, doc_attrs, type_name, vis, derive_traits),
+        InnerType::Integer(I64) => IntegerNewtype::<i64>::expand(attrs, doc_attrs, type_name, vis, derive_traits),
+        InnerType::Integer(I128) => IntegerNewtype::<i128>::expand(attrs, doc_attrs, type_name, vis, derive_traits),
+        InnerType::Integer(Isize) => IntegerNewtype::<isize>::expand(attrs, doc_attrs, type_name, vis, derive_traits),
+        InnerType::Float(F32) => FloatNewtype::<f32>::expand(attrs, doc_attrs, type_name, vis, derive_traits),
+        InnerType::Float(F64) => FloatNewtype::<f64>::expand(attrs, doc_attrs, type_name, vis, derive_traits),
     }
 }
