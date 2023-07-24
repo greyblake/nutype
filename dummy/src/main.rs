11216e7a9d45f49a2b235affb39e5ee911f9d0ca
Sergey Potapov <blake131313@gmail.com>
2023-07-24 23:18:43 +0200

    Fix clippy warnings in dummy

--- dummy/src/main.rs
+++ dummy/src/main.rs
@@ -1,8 +1,18 @@
 use nutype::nutype;
 
-#[nutype(validate(with = |x| x.is_empty() ))]
-// #[nutype(sanitize(with = trim_name ))]
-#[derive(Debug)]
-pub struct Name(String);
+#[nutype(
+    sanitize(trim, lowercase),
+    validate(not_empty),
+    derive(
+        TryFrom, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, FromStr, AsRef, Hash
+    )
+)]
+pub struct Email(String);
 
-fn main() {}
+#[nutype(derive(Deref))]
+pub struct Number(i16);
+
+fn main() {
+    let magic = Number::new(42);
+    assert_eq!(*magic, 42);
+}
