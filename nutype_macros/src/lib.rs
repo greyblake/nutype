99a2d99635baf8e1746b29157b850adf7b583b5e
Sergey Potapov <blake131313@gmail.com>
2023-08-18 07:45:00 +0200

    Rework dispatch in expand_nutype()

--- nutype_macros/src/lib.rs
+++ nutype_macros/src/lib.rs
@@ -4,8 +4,9 @@ mod float;
 mod integer;
 mod string;
 
+use any::AnyNewtype;
 use common::{
-    models::{InnerType, Newtype},
+    models::{InnerType, Newtype, TypedMeta},
     parse::meta::parse_meta,
 };
 use float::{models::FloatInnerType, FloatNewtype};
@@ -27,27 +28,47 @@ fn expand_nutype(
     attrs: TokenStream,
     type_definition: TokenStream,
 ) -> Result<TokenStream, syn::Error> {
-    use FloatInnerType::*;
-    use IntegerInnerType::*;
-
     let meta = parse_meta(type_definition)?;
     let (typed_meta, inner_type) = meta.into_typed_meta(attrs);
 
     match inner_type {
         InnerType::String(tp) => StringNewtype::expand(typed_meta, tp),
-        InnerType::Integer(U8) => IntegerNewtype::<u8>::expand(typed_meta, U8),
-        InnerType::Integer(U16) => IntegerNewtype::<u16>::expand(typed_meta, U16),
-        InnerType::Integer(U32) => IntegerNewtype::<u32>::expand(typed_meta, U32),
-        InnerType::Integer(U64) => IntegerNewtype::<u64>::expand(typed_meta, U64),
-        InnerType::Integer(U128) => IntegerNewtype::<u128>::expand(typed_meta, U128),
-        InnerType::Integer(Usize) => IntegerNewtype::<usize>::expand(typed_meta, Usize),
-        InnerType::Integer(I8) => IntegerNewtype::<i8>::expand(typed_meta, I8),
-        InnerType::Integer(I16) => IntegerNewtype::<i16>::expand(typed_meta, I16),
-        InnerType::Integer(I32) => IntegerNewtype::<i32>::expand(typed_meta, I32),
-        InnerType::Integer(I64) => IntegerNewtype::<i64>::expand(typed_meta, I64),
-        InnerType::Integer(I128) => IntegerNewtype::<i128>::expand(typed_meta, I128),
-        InnerType::Integer(Isize) => IntegerNewtype::<isize>::expand(typed_meta, Isize),
-        InnerType::Float(F32) => FloatNewtype::<f32>::expand(typed_meta, F32),
-        InnerType::Float(F64) => FloatNewtype::<f64>::expand(typed_meta, F64),
+        InnerType::Integer(inner) => expand_nutype_integer(typed_meta, inner),
+        InnerType::Float(inner) => expand_nutype_float(typed_meta, inner),
+        InnerType::Any(any_inner_type) => AnyNewtype::expand(typed_meta, any_inner_type),
+    }
+}
+
+fn expand_nutype_integer(
+    typed_meta: TypedMeta,
+    inner: IntegerInnerType,
+) -> Result<TokenStream, syn::Error> {
+    use IntegerInnerType::*;
+
+    match inner {
+        U8 => IntegerNewtype::<u8>::expand(typed_meta, U8),
+        U16 => IntegerNewtype::<u16>::expand(typed_meta, U16),
+        U32 => IntegerNewtype::<u32>::expand(typed_meta, U32),
+        U64 => IntegerNewtype::<u64>::expand(typed_meta, U64),
+        U128 => IntegerNewtype::<u128>::expand(typed_meta, U128),
+        Usize => IntegerNewtype::<usize>::expand(typed_meta, Usize),
+        I8 => IntegerNewtype::<i8>::expand(typed_meta, I8),
+        I16 => IntegerNewtype::<i16>::expand(typed_meta, I16),
+        I32 => IntegerNewtype::<i32>::expand(typed_meta, I32),
+        I64 => IntegerNewtype::<i64>::expand(typed_meta, I64),
+        I128 => IntegerNewtype::<i128>::expand(typed_meta, I128),
+        Isize => IntegerNewtype::<isize>::expand(typed_meta, Isize),
+    }
+}
+
+fn expand_nutype_float(
+    typed_meta: TypedMeta,
+    inner: FloatInnerType,
+) -> Result<TokenStream, syn::Error> {
+    use FloatInnerType::*;
+
+    match inner {
+        F32 => FloatNewtype::<f32>::expand(typed_meta, F32),
+        F64 => FloatNewtype::<f64>::expand(typed_meta, F64),
     }
 }
