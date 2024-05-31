6d5e676c201602d4ca19e7e76f19d922995483b7
Serhii Potapov <blake131313@gmail.com>
2024-05-31 21:57:20 +0200

    Fix clippy

--- dummy/src/main.rs
+++ dummy/src/main.rs
@@ -2,7 +2,7 @@ use nutype::nutype;
 
 #[nutype(
     sanitize(with = |v| v),
-    validate(predicate = |v| v.len() > 0)
+    validate(predicate = |v| !v.is_empty() )
 )]
 struct NonEmptyVec<T>(Vec<T>);
 
