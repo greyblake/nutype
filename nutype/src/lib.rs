0bc77ad171db6734a8a094fe9e1a3948d6a38704
Serhii Potapov <blake131313@gmail.com>
2024-07-06 17:44:57 +0200

    Add an example with generics in README and lib.rs doc

--- nutype/src/lib.rs
+++ nutype/src/lib.rs
@@ -243,7 +243,7 @@
 //! struct Size(f64);
 //! ```
 //!
-//! ## Other inner types
+//! ## Other inner types and generics
 //!
 //! For any other type it is possible to define custom sanitizers with `with` and custom
 //! validations with `predicate`:
@@ -252,45 +252,33 @@
 //! use nutype::nutype;
 //!
 //! #[nutype(
-//!     derive(Debug, PartialEq, Deref, AsRef),
+//!     derive(Debug, PartialEq, AsRef, Deref),
 //!     sanitize(with = |mut guests| { guests.sort(); guests }),
 //!     validate(predicate = |guests| !guests.is_empty() ),
 //! )]
 //! pub struct GuestList(Vec<String>);
 //!
-//! // Empty list is not allowed
-//! assert_eq!(
-//!     GuestList::try_new(vec![]),
-//!     Err(GuestListError::PredicateViolated)
-//! );
+//! ```
 //!
-//! // Create the list of our guests
-//! let guest_list = GuestList::try_new(vec![
-//!     "Seneca".to_string(),
-//!     "Marcus Aurelius".to_string(),
-//!     "Socrates".to_string(),
-//!     "Epictetus".to_string(),
-//! ]).unwrap();
+//! It's also possible to use generics:
 //!
-//! // The list is sorted (thanks to sanitize)
-//! assert_eq!(
-//!     guest_list.as_ref(),
-//!     &[
-//!         "Epictetus".to_string(),
-//!         "Marcus Aurelius".to_string(),
-//!         "Seneca".to_string(),
-//!         "Socrates".to_string(),
-//!     ]
-//! );
+//! ```
+//! use nutype::nutype;
 //!
-//! // Since GuestList derives Deref, we can use methods from `Vec<T>`
-//! // due to deref coercion (if it's a good idea or not, it's left up to you to decide!).
-//! assert_eq!(guest_list.len(), 4);
+//! #[nutype(
+//!     sanitize(with = |mut v| { v.sort(); v }),
+//!     validate(predicate = |vec| !vec.is_empty()),
+//!     derive(Debug, PartialEq, AsRef, Deref),
+//! )]
+//! struct SortedNotEmptyVec<T: Ord>(Vec<T>);
 //!
-//! for guest in guest_list.iter() {
-//!     println!("{guest}");
-//! }
+//! let wise_friends = SortedNotEmptyVec::try_new(vec!["Seneca", "Zeno", "Plato"]).unwrap();
+//! assert_eq!(wise_friends.as_ref(), &["Plato", "Seneca", "Zeno"]);
+//! assert_eq!(wise_friends.len(), 3);
 //!
+//! let numbers = SortedNotEmptyVec::try_new(vec![4, 2, 7, 1]).unwrap();
+//! assert_eq!(numbers.as_ref(), &[1, 2, 4, 7]);
+//! assert_eq!(numbers.len(), 4);
 //! ```
 //!
 //! ## Custom sanitizers
