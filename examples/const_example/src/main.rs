82272362fc13b55a20d80678bdc85ee13abcec6d
Serhii Potapov <blake131313@gmail.com>
2025-01-02 08:05:29 +0100

    Support const_fn for floats

--- examples/const_example/src/main.rs
+++ examples/const_example/src/main.rs
@@ -12,6 +12,25 @@ const PENSION_AGE: Age = match Age::try_new(67) {
     Err(_) => panic!("Invalid Age"),
 };
 
+// Until `const` functions are stabilized, we recommend to  define in application
+// and reuse `nutype_const!` macro to create constants from `nutype` types
+#[nutype(
+    const_fn,
+    validate(greater_or_equal = -1.0, less_or_equal = 1.0)
+)]
+struct Correlation(f32);
+
+macro_rules! nutype_const {
+    ($name:ident, $ty:ty, $value:expr) => {
+        const $name: $ty = match <$ty>::try_new($value) {
+            Ok(value) => value,
+            Err(_) => panic!("Invalid value"),
+        };
+    };
+}
+
+nutype_const!(MAX_CORRELATION, Correlation, 1.0);
+
 // Not recommended, but it's possible to use `new_unchecked` with `const_fn` flag.
 #[nutype(
     const_fn,
@@ -25,4 +44,5 @@ const DEFAULT_TAX_CLASS: TaxClass = unsafe { TaxClass::new_unchecked(1) };
 fn main() {
     assert_eq!(PENSION_AGE.into_inner(), 67);
     assert_eq!(DEFAULT_TAX_CLASS.into_inner(), 1);
+    assert_eq!(MAX_CORRELATION.into_inner(), 1.0);
 }
